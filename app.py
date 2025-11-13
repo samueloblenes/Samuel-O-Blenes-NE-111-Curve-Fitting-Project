c03ef438efb5f50fa105d86550f9c3ba01c11b0b
samueloblenes <oblenessamuel@gmail.com>
2025-11-12 19:33:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,15 +67,83 @@ st.markdown(f"<div class='description-box'>{"Select either Auto fit, or manual f
 # Create Tabs
 tab1, tab2= st.tabs(["Auto Fit", "Manual Fit"])
 
-
-########## Tab1, Auto curve fitting ##########
-with tab1:
-    #Initializing variables
+#Initializing variables
     if "Dataconfirmed" not in st.session_state:
         st.session_state.Dataconfirmed = False # Session state varaible to keep track of if the user has confirmed the entered data
     if "df" not in st.session_state:
         st.session_state.df = pd.DataFrame(columns=["X-Axis", "Y-Axis"]) # session state variable to store the data being entered
 
+
+########## Tab1 Auto curve fitting ##########
+with tab1:    
+     entry_method = st.selectbox("Choose to enter data manualy or upload a CSV file",("Manual entry","Upload CSV file"))
+
+        # Manual entry mode
+        if  entry_method == "Manual entry":
+            edited_df = st.data_editor(st.session_state.df, num_rows="dynamic") # make the data frame editable 
+
+        # File upload mode
+        elif entry_method == "Upload CSV file":
+            uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+            if uploaded_file != None:
+                df_uploaded = pd.read_csv(uploaded_file) # Read the CSV file to a pandas DataFrame
+
+                # check if dataframe has the correct dimensions
+                num_cols = df_uploaded.shape[1]
+                
+                if num_cols == 0:
+                    st.error("Uploaded CSV has no columns, please upload valid data.")
+                    
+                elif num_cols == 1: # for only 1 column treat as Y-Axis, create default X-Axis
+                    df_uploaded.columns = ['Y-Axis'] # rename columns
+                    df_uploaded['X-Axis'] = range(1, len(df_uploaded) + 1)
+                    df_uploaded = df_uploaded[['X-Axis', 'Y-Axis']]  # reorder columns
+                    st.warning("Only one column found, assumed it is Y-Axis, X-Axis assigned as sequential integers starting from 1.")
+                    edited_df = df_uploaded
+                    
+                elif num_cols == 2:
+                    df_uploaded.columns = ['X-Axis', 'Y-Axis']
+                    edited_df = df_uploaded
+                    
+                if num_cols > 2:
+                    st.error("Uploaded CSV has more than 2 columns, please enter a file containing only 2 columns for x and y data respectively")
+      
+                # Display uploaded Data
+                st.write("Uploaded Data:")
+                st.dataframe(edited_df)
+            
+        # Confirm entered data, if there is no data entered, display an error and ask the user to input data
+        col3_1, col3_2 = st.columns(2)
+        with col3_1:
+            st.write("Click confirm to update the graph")
+            confirm_clicked = st.button("Confirm")
+            if confirm_clicked:
+                # Remove rows where ALL cells are None, to check if there are actually any numerical values, not just a bunch of aded empty rows
+                cleaned_df = edited_df.dropna(how="all")
+                # Check if at least one cell is not empty
+                if not cleaned_df.empty and cleaned_df.notna().any().any():
+                    st.session_state.df = cleaned_df
+                    st.session_state.Dataconfirmed = True
+                else:
+                   st.error("Please enter some data to confirm") #if not data has been enetred (the data frame only contains None values or no values) display this error message
+                    
+        # Clear entered data
+        with col3_2:
+             st.write("Click clear to clear all entered data")
+             if st.button("Clear"):
+                st.session_state.df = pd.DataFrame(columns=edited_df.columns) # Reset pandas dataframe 
+                st.session_state.Dataconfirmed = False # Set confirmation variable to False
+                st.rerun()   
+    
+    st.divider()
+    col1, col2 = st.columns([1,3])
+    col1.subheader("Data")
+    col2.subheader("Distribution")
+    
+########## Tab2, Manual curve fitting ##########
+with tab2:
+    
     col3, col4 = st.columns(2)
     
     # Data entry section
@@ -163,50 +231,42 @@ with tab1:
         
         st.divider()
 
-    # Graph display section
-    st.divider()
-    
-    if st.session_state.Dataconfirmed and not st.session_state.df.empty: # If data is confirmed and the dataframe is not empty, display the graph and table
-        col1, col2 = st.columns([1,3])
-        col1.subheader("Data")
-        col2.subheader("Distribution")
+# Graph display section
+st.divider()
 
-        # prepare/clean entered date
-        df_to_plot = st.session_state.df.copy() # define the dataframe to plot
-        for col in df_to_plot.columns:
-            df_to_plot[col] = pd.to_numeric(df_to_plot[col], errors='coerce')
-            df_to_plot = df_to_plot.dropna()
-
-        orig_df, fit_df = fit(df_to_plot, dist_name, int(num_points))
-
-        with col1:
-            st.dataframe(orig_df)
-            st.dataframe(fit_df)
-
-        with col2:
-            # if no numerical data was entered, display and error
-            if df_to_plot.empty:
-                st.error("No numeric data available to plot.") # Error message if no data is enetred and the program proceeds to try and graph
-                
-            else:
-               
-                fig, ax = plt.subplots()
-                ax.hist(orig_df["Y-Axis"], bins=30, density=True, alpha=0.5, label="Data Histogram") # create histogram of the entered data
-                ax.plot(fit_df["X-Axis"], fit_df["Y-Axis"], color='red', lw=2, label="Fitted Curve") # Overlay the fitted curve
-                # Display in Streamlit
-                st.pyplot(fig)
-
-    else: 
-        st.write("Once you enter and confirm your data a graph will apear here") # if data is not confirmed, display this message
-
-        
-########## Tab2 Manual curve fitting ##########
-with tab2:    
-    
-    col3, col4 = st.columns(2)
-
-    st.divider()
+if st.session_state.Dataconfirmed and not st.session_state.df.empty: # If data is confirmed and the dataframe is not empty, display the graph and table
     col1, col2 = st.columns([1,3])
     col1.subheader("Data")
     col2.subheader("Distribution")
+
+    # prepare/clean entered date
+    df_to_plot = st.session_state.df.copy() # define the dataframe to plot
+    for col in df_to_plot.columns:
+        df_to_plot[col] = pd.to_numeric(df_to_plot[col], errors='coerce')
+        df_to_plot = df_to_plot.dropna()
+    
+    orig_df, fit_df = fit(df_to_plot, dist_name, int(num_points))
+    
+    with col1:
+        st.dataframe(orig_df)
+        st.dataframe(fit_df)
+    
+    with col2:
+        # if no numerical data was entered, display and error
+        if df_to_plot.empty:
+            st.error("No numeric data available to plot.") # Error message if no data is enetred and the program proceeds to try and graph
+            
+        else:
+           
+            fig, ax = plt.subplots()
+            ax.hist(orig_df["Y-Axis"], bins=30, density=True, alpha=0.5, label="Data Histogram") # create histogram of the entered data
+            ax.plot(fit_df["X-Axis"], fit_df["Y-Axis"], color='red', lw=2, label="Fitted Curve") # Overlay the fitted curve
+            # Display in Streamlit
+            st.pyplot(fig)
+    
+else: 
+    st.write("Once you enter and confirm your data a graph will apear here") # if data is not confirmed, display this message
+
+        
+
 
